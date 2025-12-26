70b1af643faa3f3b64fbe27ad04e8062b6b28095
Sajawal Baig <sajawalbaig007@gmail.com>
2025-12-26 21:55:21 +0500

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import Header from '../components/Header'
+
 import Hero from '../components/Hero'
-import Reserve from '../components/Reserve'
-import Footer from '../components/Footer'
+
 import Food from '../components/Food'
 
 function Home() {
